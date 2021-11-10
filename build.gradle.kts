16056aa6c7546331710be6cc5cda1d72ce058b89
akiescarraga <aki_escarraga@dlsu.edu.ph>
2021-11-10 14:57:33 +0800

    Added Apache Common Labs dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,13 @@ plugins {
     java
 }
 
-group = "com.orangeandbronze.enlistment"
-version = "1.0-SNAPSHOT"
-
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/org.apache.commons/commons-lang3
+    implementation("org.apache.commons:commons-lang3:3.12.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
