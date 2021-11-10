1bf7c6521acaed0af75e1d68f24bd3e25127d663
akiescarraga <aki_escarraga@dlsu.edu.ph>
2021-11-10 16:59:12 +0800

    added equals() and hashCode() for Room

--- src/main/java/com/stswengists/Room.java
+++ src/main/java/com/stswengists/Room.java
@@ -1,20 +1,38 @@
 package com.stswengists;
 
-import org.apache.commons.lang3.*;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
+
+import java.util.Objects;
+
+import static org.apache.commons.lang3.Validate.notBlank;
 
 public class Room {
     private String roomName;
     private final int maxCapacity;
-    private int currCapacity;
+    private int currentCapacity;
 
-    public Room(String roomName, int maxCapacity) {
+    public Room(String roomID, String roomName, int maxCapacity) {
         notBlank(roomName,
                 "Room name cannot be blank, empyt or whitespace");
-        Validate.isTrue(StringUtils.isAlphaNumeric(roomName),
+        Validate.isTrue(StringUtils.isAlphanumeric(roomName),
                 "Room Name must be alphanumeric, was: " + roomName);
 
         this.roomName = roomName;
         this.maxCapacity = maxCapacity;
-        this.currCapacity = currCapacity;
+        this.currentCapacity = 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Room room = (Room) o;
+        return roomName.equals(room.roomName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roomName);
     }
 }
