1666f6ad2144f74ad8852fb200042559150ceaf3
akiescarraga <aki_escarraga@dlsu.edu.ph>
2021-11-10 16:19:11 +0800

    created Room class

---
+++ src/main/java/com/stswengists/Room.java
@@ -0,0 +1,11 @@
+package com.stswengists;
+
+public class Room {
+    private String roomName;
+    private final int capacity;
+
+    public Room(String roomName, int capacity) {
+        this.roomName = roomName;
+        this.capacity = capacity;
+    }
+}
