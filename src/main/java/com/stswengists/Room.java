6051de9526b916c6974f89db1a88e1ceb33bf416
alennacayton <cayton.alenna@gmail.com>
2021-11-10 16:48:48 +0800

    added alphanumeric validation

--- src/main/java/com/stswengists/Room.java
+++ src/main/java/com/stswengists/Room.java
@@ -1,11 +1,20 @@
 package com.stswengists;
 
+import org.apache.commons.lang3.*;
+
 public class Room {
     private String roomName;
-    private final int capacity;
+    private final int maxCapacity;
+    private int currCapacity;
 
-    public Room(String roomName, int capacity) {
+    public Room(String roomName, int maxCapacity) {
+        notBlank(roomName,
+                "Room name cannot be blank, empyt or whitespace");
+        Validate.isTrue(StringUtils.isAlphaNumeric(roomName),
+                "Room Name must be alphanumeric, was: " + roomName);
+
         this.roomName = roomName;
-        this.capacity = capacity;
+        this.maxCapacity = maxCapacity;
+        this.currCapacity = currCapacity;
     }
 }
